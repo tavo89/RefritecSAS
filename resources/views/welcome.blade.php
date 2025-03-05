28cf00aac59eecac9d5a4330ffe7ba6b8eb38387
Gustavo Bocanegra <gustavo_bocanegra@hotmail.com>
2025-03-05 04:35:28 +0000

    camios produccion

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,10 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Refritec</title>
+
+        <!-- pwa -->
+        <link rel="manifest" href="/manifest.json">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
